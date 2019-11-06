e9cfd95f2dc35eff128fe85010f6110e96f418d8
Bíró Júlia <liasapiens@gmail.com>
2019-11-06 20:30:46 +0100

    id-k az a-n és a b-n to do da magic

--- szamologep_use.py
+++ szamologep_use.py
@@ -9,6 +9,13 @@ driver.find_element(By.NAME, "b").send_keys("32")
 header_text = driver.find_element(By.XPATH, "//h1").text
 assert header_text == "Szamologep"
 
-driver.find_element(By.XPATH, "//input[3]").click
 print(header_text)
 assert header_text == "Szamologep"
+
+driver.find_element(By.XPATH, "//input[3]").click()
+
+result = driver.find_element (By.ID, "result-input").click()
+
+drive.save_screenshot ("result.png")
+
+assert result == "55"
