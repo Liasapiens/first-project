d35118566b37c60fc6e046a65c82fb8f0dfd4a8b
Bíró Júlia <liasapiens@gmail.com>
2019-11-20 20:55:16 +0100

    Delete függvény helye

---
+++ docs/delete_employee.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+driver = webdriver.Chrome()
+driver.get("http://www.learnwebservices.com/empapp/employees.xhtml")
+
+def delete_by_name():
+    delete_by_name()
+
+
+
+def find_id_by_name():
